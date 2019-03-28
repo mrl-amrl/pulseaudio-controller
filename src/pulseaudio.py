ed565f856226f1b4662f7ffdf2ccabb9c6c143e8
Ahmadreza Zibaei <ahmadrezazibaei@hotmail.com>
2019-03-28 15:29:26 +0430

    some methods added to  pulseaudio

---
+++ src/pulseaudio.py
@@ -0,0 +1,109 @@
+import time
+import subprocess
+
+
+def list_sources():
+    process = subprocess.Popen('pactl list sources', shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    out, _ = process.communicate()
+
+    sources = []
+    lines = out.split('\n')
+
+    last_source = {}
+    for line in lines:
+        line = line.strip()
+
+        if line.startswith('Source #'):
+            sink_number = line[7:]
+            if last_source.get('is_ready', False):
+                sources.append(last_source)
+                last_source = {}
+
+            last_source["number"] = sink_number
+
+        if line.startswith('Name: '):
+            name = line[6:]
+            last_source["name"] = name
+        if line.startswith('device.description = "'):
+            description = line[22:-1]
+            last_source["description"] = description
+            last_source["is_ready"] = True
+
+    last_source["is_ready"] = True
+    sources.append(last_source)
+    return sources
+
+def list_sinks():
+    process = subprocess.Popen('pactl list sinks', shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    out, _ = process.communicate()
+
+    sinks = []
+    lines = out.split('\n')
+
+    last_sink = {}
+    for line in lines:
+        line = line.strip()
+
+        if line.startswith('Sink #'):
+            sink_number = line[6:]
+            if last_sink.get('is_ready', False):
+                sinks.append(last_sink)
+                last_sink = {}
+
+            last_sink["number"] = sink_number
+
+        if line.startswith('Name: '):
+            name = line[6:]
+            last_sink["name"] = name
+        if line.startswith('device.description = "'):
+            description = line[13:-1]
+            last_sink["description"] = description
+            last_sink["is_ready"] = True
+
+    last_sink["is_ready"] = True
+    sinks.append(last_sink)
+    return sinks
+
+
+def set_default_sink(name):
+    process = subprocess.Popen('pacmd set-default-sink ' + name,
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    process.communicate()
+
+
+def set_default_source(name):
+    process = subprocess.Popen('pacmd set-default-source ' + name,
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    process.communicate()
+
+
+def submit_module(module, mode, *args):
+    command = [
+        "pactl", mode + "-module", module,
+    ]
+    if args and mode == 'load':
+        command.extend(args)
+
+    time.sleep(0.1)
+
+    cmd = " ".join(command)
+    rospy.loginfo('Executing {} ...'.format(cmd))
+
+    process = subprocess.Popen(cmd, shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    if err:
+        rospy.logerr(err[:-1])
+    elif out:
+        rospy.loginfo(out[:-1])
+
+    return process.returncode
--- src/pulseaudio_node.py
+++ src/pulseaudio_node.py
@@ -1,32 +1,7 @@
 #!/usr/bin/env python2
 import rospy
-import time
-import subprocess
+import pulseaudio
 from std_srvs.srv import SetBool, SetBoolResponse
-
-
-def submit_module(module, mode, *args):
-    command = [
-        "pactl", mode + "-module", module,
-    ]
-    if args and mode == 'load':
-        command.extend(args)
-
-    time.sleep(0.1)
-
-    cmd = " ".join(command)
-    rospy.loginfo('Executing {} ...'.format(cmd))
-
-    process = subprocess.Popen(cmd, shell=True,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    out, err = process.communicate()
-    if err:
-        rospy.logerr(err[:-1])
-    elif out:
-        rospy.loginfo(out[:-1])
-
-    return process.returncode
 
 
 class Controller:
@@ -43,14 +18,14 @@ class Controller:
         resp = SetBoolResponse()
         resp.success = False
 
-        if submit_module(
+        if pulseaudio.submit_module(
             'module-zeroconf-discover',
             'load' if data else 'unload',
         ) != 0:
             resp.message = "Couldn't load module zeroconf discover"
             return resp
 
-        if submit_module(
+        if pulseaudio.submit_module(
             'module-native-protocol-tcp',
             'load' if data else 'unload',
             'auth-anonymous=1',
@@ -58,7 +33,7 @@ class Controller:
             resp.message = "Couldn't load module native protocol"
             return resp
 
-        if submit_module(
+        if pulseaudio.submit_module(
             'module-zeroconf-publish',
             'load' if data else 'unload',
         ) != 0:
